ff415bacb96ac66f0e202baf2e7b9b75e0e8cb4f
Wajed Isleem <wajedbisleem@gmail.com>
2025-07-09 04:35:30 -0600

    Adjust ProductImage component styles for consistency: update max-height to 350px and set width to 350px.

--- client/src/pages/product/partials/ProductImage.jsx
+++ client/src/pages/product/partials/ProductImage.jsx
@@ -1,6 +1,7 @@
 import styles from './ProductImage.module.css';
 
 const ProductImage = ({ product }) => {
+
   return <img src={`${product.image}?w=350`} className={styles['product-image']} alt={product.name} />;
 };
 export { ProductImage };
