a6b59309196b72924532a94908beae6e032b8c12
Wajed Isleem <wajedbisleem@gmail.com>
2025-07-09 02:40:09 -0600

    Enhance product retrieval in ProductController by adding field selection for offer, new, and featured products

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -2,17 +2,17 @@ import Product from '../database/schemas/product.schema.js';
 
 class ProductController {
   static async offer(req, res) {
-    const products = await Product.find({ offer: true });
+    const products = await Product.find({ offer: true }).select('_id en_name ar_name en_description ar_description en_category ar_category price image');
     return res.status(200).json(products);
   }
 
   static async new(req, res) {
-    const products = await Product.find({ new: true });
+    const products = await Product.find({ new: true }).select('_id en_name ar_name en_category ar_category price image stock');
     return res.status(200).json(products);
   }
 
   static async featured(req, res) {
-    const products = await Product.find({ featured: true });
+    const products = await Product.find({ featured: true }).select('_id en_name ar_name en_category ar_category price image stock');
     return res.status(200).json(products);
   }
 
