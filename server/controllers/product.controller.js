97da90596e042e504f2fa41168a0baedc3815a81
Wajed Isleem <wajedbisleem@gmail.com>
2025-07-09 06:05:57 -0600

    Enhance search functionality in ProductController to support multilingual queries and sorting options

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -17,7 +17,47 @@ class ProductController {
   }
 
   static async search(req, res) {
-    const products = await Product.find({});
+    const { search, category, sort } = req.query;
+    
+    let filter = {};
+    if (search) {
+      switch (req.lang) {
+        case 'en':
+          filter.$or = [{ en_name: { $regex: search, $options: 'i' } }, { en_description: { $regex: search, $options: 'i' } }];
+          break;
+        case 'ar':
+          filter.$or = [{ ar_name: { $regex: search, $options: 'i' } }, { ar_description: { $regex: search, $options: 'i' } }];
+          break;
+        default:
+          filter.$or = [
+            { en_name: { $regex: search, $options: 'i' } },
+            { en_description: { $regex: search, $options: 'i' } },
+            { ar_name: { $regex: search, $options: 'i' } },
+            { ar_description: { $regex: search, $options: 'i' } }
+          ];
+      }
+    }
+
+    if (category) {
+      filter.en_category = { $regex: category, $options: 'i' };
+    }
+
+    let sortOptions = {};
+    switch (sort) {
+      case 'alphabetically':
+        sortOptions = req.lang === 'en' ? { en_name: 1 } : { ar_name: 1 };
+        break;
+      case 'price-low-high':
+        sortOptions = { price: 1 };
+        break;
+      case 'price-high-low':
+        sortOptions = { price: -1 };
+        break;
+      default:
+        sortOptions = {};
+    }
+
+    const products = await Product.find(filter).sort(sortOptions).select('_id en_name ar_name en_category ar_category price image stock');
     return res.status(200).json(products);
   }
 
